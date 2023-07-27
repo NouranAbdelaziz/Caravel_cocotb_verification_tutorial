56aee461b19b5ae09e3e6afafbf7a4f734e1edb4
NouranAbdelaziz <79912650+NouranAbdelaziz@users.noreply.github.com>
2023-07-27 12:06:16 +0300

    Update gpio_test.c

--- gpio_test.c
+++ gpio_test.c
@@ -1,15 +1,12 @@
-#include <common.h> // include required APIs
+#include <firmware_apis.h> // include required APIs
 void main(){
-   // Enable managment gpio as output to use as indicator for finishing configuration 
-   mgmt_gpio_o_enable();
-   mgmt_gpio_wr(0);
-   enable_hk_spi(0); // disable housekeeping spi
-   //configure_all_gpios(GPIO_MODE_MGMT_STD_OUT);
-   configure_all_gpios(GPIO_MODE_MGMT_STD_OUTPUT);
-   gpio_config_load();
-   set_gpio_l(0x8F);
-   mgmt_gpio_wr(1); // configuration finished 
-
-   
-   return;
+ // Enable managment gpio as output to use as indicator for finishing configuration 
+  ManagmentGpio_outputEnable();
+  ManagmentGpio_write(0);
+  enableHkSpi(0); // disable housekeeping spi
+  GPIOs_configureAll(GPIO_MODE_MGMT_STD_OUTPUT);  //GPIOs_configureAll(GPIO_MODE_MGMT_STD_OUT);
+  GPIOs_loadConfigs();
+  GPIOs_writeLow(0x8F);
+  ManagmentGpio_write(1); // configuration finished 
+  return;
 }
